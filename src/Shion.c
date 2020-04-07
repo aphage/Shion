8dda86f201b65076a3be111c57a84a420922f1ca
aphage <diyloli@outlook.com>
2020-04-07 21:59:26 +0800

    update

--- src/Shion.c
+++ src/Shion.c
@@ -2,7 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2016 DarthTon
-Copyright (c) 2020 aphage
+Copyright (c) 2020 Misaka Mikoto (aphage)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
